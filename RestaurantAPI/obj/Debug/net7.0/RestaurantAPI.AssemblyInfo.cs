98f8a03e0f3ea02a37afd54c118d49fc392257a1
Juninho <juninhopeter1@gmail.com>
2024-11-18 14:26:12 +0100

    modified project folder

--- RestaurantAPI/obj/Debug/net7.0/RestaurantAPI.AssemblyInfo.cs
+++ RestaurantAPI/obj/Debug/net7.0/RestaurantAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1030f8b3958ef53c57417540db6a409392d9a2b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
